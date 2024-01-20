6cbe262369a3b7a490652cb048a2f216a6881d29
Aidan Gu <asifiazadegu@gmail.com>
2024-01-20 11:27:13 -0800

    pages

--- popup.js
+++ popup.js
@@ -1,13 +1,19 @@
-// Get the elements by their ID
-var popupLink = document.getElementById("popup-link");
-var popupWindow = document.getElementById("popup-window");
-var closeButton = document.getElementById("close-button");
-// Show the pop-up window when the link is clicked
-popupLink.addEventListener("click", function(event) {
-    event.preventDefault();
-    popupWindow.style.display = "block";
-});
-// Hide the pop-up window when the close button is clicked
-closeButton.addEventListener("click", function() {
-    popupWindow.style.display = "none";
+document.addEventListener("DOMContentLoaded", function() {
+    function openPopup() {
+        var popupWindow = document.getElementById("popup-window");
+        popupWindow.style.display = "block";
+    }
+
+    function closePopup() {
+        var popupWindow = document.getElementById("popup-window");
+        popupWindow.style.display = "none";
+    }
+
+    // Get the elements by their ID
+    var closeButton = document.getElementById("close-button");
+
+    // Hide the pop-up window when the close button is clicked
+    closeButton.addEventListener("click", function() {
+        closePopup();
+    });
 });
