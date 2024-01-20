803d365c57a59d850f7d90b3f72d247c626ccca8
Aidan Gu <asifiazadegu@gmail.com>
2024-01-19 20:05:01 -0800

    HomePage.js
    Home Page using React Outline

---
+++ HomePage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Big Name</h1>
+      <div>
+        <h2>Bio/About</h2>
+        <p>Bio Description</p>
+      </div>
+      <div>
+        <h2>Mission Statement</h2>
+        <p>write mission statement</p>
+      </div>
+      <div>
+        <h2>Creators</h2>
+        <p>omg aidan eli and hannah!.</p>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
